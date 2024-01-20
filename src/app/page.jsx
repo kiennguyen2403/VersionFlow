7de171c2bd3255644df67f7bf6f7e70ab3716aa5
Kien Nguyen <kien.nguyen@moveconnect.com>
2024-01-20 14:49:11 +1100

    working on custom graph

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -32,29 +32,7 @@ export default async function Page() {
 
   return (
     <div>
-
       <CustomCommitTree />
-      {/* <h3>API usage demo</h3>
-      <p className="p-small">API Calls need to be authenticated</p>
-      <p>
-        Apps that use the API usually would run on your own domain. During
-        development, test on http://localhost:3000
-      </p>
-      {authUrl ? (
-        <a className="button button-primary" href={authUrl}>
-          Login
-        </a>
-      ) : (
-        <>
-          <p>This is a list of all the boards that your user has access to:</p>
-
-          <ul>
-            {boards?.map((board) => (
-              <li key={board.name}>{board.name}</li>
-            ))}
-          </ul>
-        </>
-      )} */}
     </div>
   );
 }
--- src/components/custom-commit-tree.jsx
+++ src/components/custom-commit-tree.jsx
@@ -1,71 +1,40 @@
 'use client';
 import React, {useState, useEffect} from 'react';
+import { Gitgraph } from '@gitgraph/react';
 import { CommitGraph } from 'commit-graph';
-
-
-const commitsData = [
-  {
-    hash: "commit-hash",
-    ownerName: "repository-owner",
-    repoName: "repository-name",
-    committer: {
-      displayName: "committer-displayName",
-    },
-    message: "commit-message",
-    parents: ["parent-commit-hash-1", "parent-commit-hash-2"],
-    committedAt: new Date(),
-    commitLink:
-      "https://github.com/repository-owner/repository-name/main/commit-hash",
-  },
-  {
-    hash: "commit-hash",
-    ownerName: "repository-owner",
-    repoName: "repository-name",
-    committer: {
-      displayName: "committer-displayName",
-    },
-    message: "commit-message",
-    parents: ["parent-commit-hash-1", "parent-commit-hash-2"],
-    committedAt: new Date(),
-    commitLink:
-      "https://github.com/repository-owner/repository-name/main/commit-hash",
-  },
-];
-
-const branchesHeads = [{
-    branchName: "branch-name-1",
-    headCommitHash: "commit-hash-1",
-  },
-  {
-    branchName: "branch-name-2",
-    headCommitHash: "commit-hash-2",
-    branchLink: "https://github.com/repository-owner/repository-name/main",
-}];
-
 
 export const CustomCommitTree = () => {
     const [commits, setCommits] = useState([]);
     const [branches, setBranches] = useState([]);
 
-    useEffect(() => {
-        setCommits(commitsData);
-        setBranches(branchesHeads); 
-    }, []);
+
 
     return (
-      <div style={{
-        margin: "auto",
-      }}>
-        <CommitGraph
-          commits={commits}
-          branchHeads={branches}
-          graphStyle={{
-            commitSpacing: 50,
-            branchSpacing: 20,
-            branchColors: ["#FF0000", "#00FF00", "#0000FF"],
-            nodeRadius: 2,
+      <div
+        style={{
+          width: "50%",
+        }}>
+        <Gitgraph>
+          {(gitgraph) => {
+            // Simulate git commands with Gitgraph API.
+            const master = gitgraph.branch("master");
+            master.commit("Initial commit");
+
+            const develop = master.branch("develop");
+            develop.commit("Add TypeScript");
+
+            const aFeature = develop.branch("a-feature");
+            aFeature
+              .commit("Make it work")
+              .commit("Make it right")
+              .commit("Make it fast");
+
+            develop.merge(aFeature);
+            develop.commit("Prepare v1");
+
+            master.merge(develop).tag("v1.0.0");
           }}
-        />
+        </Gitgraph>
       </div>
     );
 }
